8d19fee198e9b2a225f43afa4c3a8bfb5c8900c8
Naveen Sharma <103960076+NavGithub15@users.noreply.github.com>
2022-09-15 10:54:38 -0400

    Made changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
